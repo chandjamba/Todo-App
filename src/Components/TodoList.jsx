1952a03a892541749644ef24af7cf1b701b4fdbb
chandjamba <chandjamba2001@gmail.com>
2024-10-19 06:59:12 -0400

    Working with functions and styling

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -3,7 +3,7 @@ import styles from "./todoList.module.css";
 export default function TodoList({ todos, setTodos }) {
   const sortedTodos = todos.sort((a, b) => b.done - a.done);
   return (
-    <div className={styles.list}>
+    <div className={styles.list }>
       {sortedTodos.map((item) => (
         <TodoItem
           key={item.name}
